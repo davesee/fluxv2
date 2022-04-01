216c730ae7e12244bd68e5e997ea1b36139235fc
davesee <davesee@microsoft.com>
2022-03-31 20:45:17 -0400

    fasd

--- automate.sh
+++ automate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-helm template -f ./sources/rabbitmq/values.yaml ./sources/rabbitmq --namespace phoenix-stage >./kustomize/infra/rabbitmq.yaml
+helm template -f ./sources/rabbitmq/values.yaml ./sources/rabbitmq --name-template=stage --namespace phoenix-stage >./kustomize/infra/rabbitmq.yaml
 
-helm template -f ./sources/edgesql/values.yaml ./sources/edgesql --namespace phoenix-stage >./kustomize/infra/edgesql.yaml
+helm template -f ./sources/edgesql/values.yaml ./sources/edgesql --name-template=stage --namespace phoenix-stage >./kustomize/infra/edgesql.yaml
+
+helm template -f ./sources/mssql/values.yaml ./sources/mssql --name-template=stage --namespace phoenix-stage >./kustomize/infra/mssql.yaml
