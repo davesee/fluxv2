ed43918bc5088114916845fd9571dc0b27bbe873
davesee <davesee@microsoft.com>
2022-04-03 18:09:42 -0400

    kljo;l

--- automate.sh
+++ automate.sh
@@ -9,7 +9,7 @@ main() {
     # create public chart yamls
     import_source infra rabbitmq https://charts.bitnami.com/bitnami bitnami/rabbitmq
     import_source monitoring grafana https://grafana.github.io/helm-charts grafana/grafana \
-        "testFramework.enabled=false"
+        "testFramework.enabled=false,service.type=LoadBalancer"
     import_source monitoring prometheus https://prometheus-community.github.io/helm-charts prometheus-community/prometheus \
         "nodeExporter.enabled=false,pushgateway.enabled=false,alertmanager.enabled=false"
 
