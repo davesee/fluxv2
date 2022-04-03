8428c6ee22cb9da916436c1604c16daf9feb87a7
davesee <davesee@microsoft.com>
2022-04-03 00:13:12 -0400

    sdfgerf

--- automate.sh
+++ automate.sh
@@ -11,7 +11,7 @@ main() {
     import_source monitoring kube-state-metrics https://prometheus-community.github.io/helm-charts prometheus-community/kube-state-metrics
 
     # create custom chart yamls
-    helm template -f ./sources/edgesql/values.yaml ./sources/edgesql --name-template=edgesql --namespace patchme >./kustomize/base/infra/edgesql.yaml
+    helm template -f ./sources/edgesql/values.yaml ./sources/edgesql --name-template=edgesql --namespace patchme >./kustomize/bases/infra/edgesql.yaml
 }
 
 import_source() {
@@ -21,16 +21,19 @@ import_source() {
     local chart=$4
 
     # remove previous files
-    rm -rf ./sources/$name ./kustomize/base/$base_type/$name.yaml
+    rm -rf ./sources/$name ./kustomize/bases/$base_type/$name.yaml
     mkdir -p ./sources/$name
 
     # add the repo and pull the chart
     silent=$(helm repo add $name $helm_repo)
     helm pull $chart -d ./sources/$name
     version=$(ls ./sources/$name)
+    silent=$(helm repo remove $name)
 
     # create the complete chart in one yaml file
-    helm template "./sources/$name/$version" --name-template=$name --namespace patchme >./kustomize/base/$base_type/$name.yaml
+    helm template "./sources/$name/$version" --name-template=$name --namespace patchme >./kustomize/bases/$base_type/$name.yaml
+
+    rm -rf ./sources/$name
 }
 
 main
